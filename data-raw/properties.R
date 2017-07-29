ee129bd133141648916b2817677e2a5572a1d687
Ben Raymond <ben.raymond@aad.gov.au>
2017-07-29 20:42:44 +1000

    add standard length as property

--- data-raw/properties.R
+++ data-raw/properties.R
@@ -3,6 +3,7 @@ sol_properties_data <- tribble(~property,~units,~class_name,
                                "lower hood length","mm","solprop_LHL",
                                "crest length","mm","solprop_CL",
                                "mantle length","mm","solprop_ML",
+                               "standard length","mm","solprop_SL",
                                "mass","g","solprop_mass",
                                "otolith length","mm","solprop_OL",
                                "otolith width","mm","solprop_OW")
