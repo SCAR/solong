5fee394955c7ccdfc4eabb6d3bd41023b68750b6
Ben Raymond <ben.raymond@aad.gov.au>
2017-07-30 12:00:24 +1000

    add hood length

--- data-raw/properties.R
+++ data-raw/properties.R
@@ -1,6 +1,8 @@
+library(dplyr)
 sol_properties_data <- tribble(~property,~units,~class_name,
                                "lower rostral length","mm","solprop_LRL",
                                "lower hood length","mm","solprop_LHL",
+                               "hood length","mm","solprop_HL",
                                "crest length","mm","solprop_CL",
                                "mantle length","mm","solprop_ML",
                                "standard length","mm","solprop_SL",
@@ -8,4 +10,4 @@ sol_properties_data <- tribble(~property,~units,~class_name,
                                "otolith length","mm","solprop_OL",
                                "otolith width","mm","solprop_OW")
 
-use_data(sol_properties_data,internal=FALSE,overwrite=TRUE)
+devtools::use_data(sol_properties_data,internal=FALSE,overwrite=TRUE)
