deb3437ddbc425f2cb3d1c22d317ef3d3dcb238b
Ben Raymond <ben.raymond@aad.gov.au>
2017-07-31 14:31:11 +1000

    dplyr imports

--- R/solong.R
+++ R/solong.R
@@ -3,7 +3,7 @@
 #' @name solong
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.number is.string
-#' @importFrom dplyr %>% arrange_ do filter_ group_by_ group_indices_ mutate_ pull select_ tibble ungroup
+#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ tibble ungroup
 #' @importFrom units ud_units
 #' @importFrom utils globalVariables
 NULL
