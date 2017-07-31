1cccb712663a762b320c15e27c26b94b9302ec42
Ben Raymond <ben.raymond@aad.gov.au>
2017-07-31 14:32:45 +1000

    carapace and total length

--- data-raw/properties.R
+++ data-raw/properties.R
@@ -6,8 +6,11 @@ sol_properties_data <- tribble(~property,~units,~class_name,
                                "crest length","mm","solprop_CL",
                                "mantle length","mm","solprop_ML",
                                "standard length","mm","solprop_SL",
+                               "carapace length","mm","solprop_CPL",
+                               "total length","mm","solprop_TL",
                                "mass","g","solprop_mass",
                                "otolith length","mm","solprop_OL",
                                "otolith width","mm","solprop_OW")
 
+assert_that(!any(duplicated(sol_properties_data$class_name)))
 devtools::use_data(sol_properties_data,internal=FALSE,overwrite=TRUE)
