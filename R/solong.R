a6004c41adf2a8a2750b55e5cf5e211081eaae5b
Ben Raymond <ben.raymond@aad.gov.au>
2017-08-01 10:16:53 +1000

    import digest

--- R/solong.R
+++ R/solong.R
@@ -3,6 +3,7 @@
 #' @name solong
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.number is.string
+#' @importFrom digest digest
 #' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ tibble ungroup
 #' @importFrom units ud_units
 #' @importFrom utils globalVariables
