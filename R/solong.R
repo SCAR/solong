9f417deaf4f6d1e1d0ce78828d3e2a61d01c6e1e
Ben Raymond <ben.raymond@aad.gov.au>
2017-08-02 09:28:44 +1000

    import tibble

--- R/solong.R
+++ R/solong.R
@@ -4,7 +4,8 @@
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.number is.string
 #' @importFrom digest digest
-#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ tibble tribble ungroup
+#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ ungroup
+#' @importFrom tibble obj_sum type_sum tibble tribble
 #' @importFrom units ud_units
 #' @importFrom utils globalVariables
 NULL
