298ce8be6e70994e8162b917ba0d117f05b4a24a
Ben Raymond <ben.raymond@aad.gov.au>
2017-08-02 09:51:02 +1000

    nah, don't need tibble package yet

--- R/solong.R
+++ R/solong.R
@@ -4,8 +4,7 @@
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.number is.string
 #' @importFrom digest digest
-#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ ungroup
-#' @importFrom tibble obj_sum type_sum tibble tribble
+#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull select_ tibble tribble ungroup
 #' @importFrom units ud_units
 #' @importFrom utils globalVariables
 NULL
