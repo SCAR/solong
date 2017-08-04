62587f38ac6bac67d2189e04b0d48df1955bcf59
Ben Raymond <ben.raymond@aad.gov.au>
2017-08-04 15:02:32 +1000

    add missing imports

--- R/solong.R
+++ R/solong.R
@@ -4,7 +4,9 @@
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.number is.string
 #' @importFrom digest digest
-#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ mutate_ mutate_all pull rowwise select_ summarize_ tibble tribble ungroup
+#' @importFrom dplyr %>% arrange_ bind_cols do filter_ group_by_ group_indices_ left_join
+#' @importFrom dplyr mutate_ mutate_all pull rowwise select_ summarize_ tibble tribble ungroup
+#' @importFrom stats na.omit
 #' @importFrom units ud_units
 #' @importFrom utils bibentry globalVariables
 NULL
