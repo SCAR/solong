228fce0fa055d0f5c60f4b4ea688e34c0d9662d5
Ben Raymond <ben.raymond@aad.gov.au>
2021-04-26 11:40:14 +1000

    add filters to fishbase

--- R/fishbase.R
+++ R/fishbase.R
@@ -5,23 +5,25 @@
 #' Experimental. Requires the rfishbase package to be installed.
 #'
 #' @param ... : arguments passed to rfishbase::length_weight
+#' @param input_properties character: an optional vector of properties (see \code{link{sol_properties}}). Only equations that take an input in \code{input_properties} will be returned
+#' @param return_properties character: an optional vector of properties (see \code{link{sol_properties}}). Only equations that return a value in \code{return_properties} will be returned
 #' @param worms logical: if TRUE, and if the worrms package is installed, try and find the AphiaID for the taxon in the World Register of Marine Species
 #'
-#' @return equation object
+#' @return A tibble of equation(s)
 #'
 #' @seealso \code{\link{sol_equation}} \code{\link{sol_equations}}
 #'
 #' @examples
 #' \dontrun{
 #'  library(dplyr)
-#'  eq <- sol_fb_length_weight("Electrona antarctica")
+#'  eq <- sol_fb_length_weight("Electrona antarctica", input_properties = "standard length")
 #'  x <- tibble(SL = 10) %>%
 #'    mutate(SL = sol_set_property(SL, "standard length", with_units = "cm"))
-#'  sol_allometry(x, eq[1, ])
+#'  sol_allometry(x, eq)
 #' }
 #'
 #' @export
-sol_fb_length_weight <- function(...,worms=requireNamespace("worrms",quietly=TRUE)) {
+sol_fb_length_weight <- function(..., input_properties, return_properties, worms = requireNamespace("worrms", quietly = TRUE)) {
     assert_that(is.flag(worms))
     if (!requireNamespace("rfishbase",quietly=TRUE))
         stop("install the rfishbase package to use this function")
@@ -30,7 +32,7 @@ sol_fb_length_weight <- function(...,worms=requireNamespace("worrms",quietly=TRU
         worms <- FALSE
     }
     x <- rfishbase::length_weight(...)
-    if (nrow(x)<1) return(NULL)
+    if (nrow(x) < 1) return(NULL)
     thisrefs <- NULL
     if (!all(is.na(x$DataRef))) {
         suppressMessages(thisrefs <- rfishbase::references(x$DataRef[!is.na(x$DataRef)], fields = c("RefNo", "Author", "Year", "Title", "Source", "ShortCitation")))
@@ -50,7 +52,7 @@ sol_fb_length_weight <- function(...,worms=requireNamespace("worrms",quietly=TRU
         eval(parse(text=sprintf("function(L) tibble(allometric_value=%g*(L^%g))",a,b)))
     }
 
-    bind_rows(lapply(seq_len(nrow(x)),function(z) {
+    zz <- bind_rows(lapply(seq_len(nrow(x)),function(z) {
         this <- x[z,]
         suppressWarnings(
         out <- sol_make_equation(equation_id=paste0("fishbase::",this$AutoCtr),
@@ -71,6 +73,15 @@ sol_fb_length_weight <- function(...,worms=requireNamespace("worrms",quietly=TRU
                                             "R^2",this$CoeffDetermination)
         out
     }))
+    ## additional filters
+    if (!missing(return_properties) && length(return_properties) > 0) {
+        zz <- dplyr::filter(zz, .data$return_property %in% return_properties)
+    }
+    if (!missing(input_properties) && length(input_properties) > 0) {
+        ok <- vapply(seq_len(nrow(zz)), function(z) any(zz$inputs[[z]]$property %in% input_properties), FUN.VALUE = TRUE)
+        zz <- zz[ok, ]
+    }
+    zz
 }
 
 
--- tests/testthat/test_fishbase.R
+++ tests/testthat/test_fishbase.R
@@ -2,11 +2,11 @@ context("solong")
 
 test_that("fishbase stuff works", {
     skip_if_not_installed("rfishbase")
-    eq <- sol_fb_length_weight("Electrona antarctica")
-    expect_gt(nrow(eq), 1)
+    eq <- sol_fb_length_weight("Electrona antarctica", input_properties = "standard length")
+    expect_equal(nrow(eq), 1)
     x <- tibble(SL = 10) %>% mutate(SL = sol_set_property(SL, "standard length", with_units = "cm"))
 
-    xa <- sol_allometry(x, eq[1, ])
+    xa <- sol_allometry(x, eq)
     expect_true(as.numeric(xa$allometric_value) > 10) ## > 10g
 
     eq <- sol_fb_length_weight("Pleuragramma antarctica")
